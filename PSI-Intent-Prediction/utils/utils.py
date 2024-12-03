d57583b5204e127aede3c71cfbc96ded60fdd0a7
Dydy <85658210+dydydahady@users.noreply.github.com>
2024-12-03 13:54:01 +0800

    Latest working iteration
    Transformer + Class Weights + Focal Loss

---
+++ PSI-Intent-Prediction/utils/utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def save_args(self, args):
+    # 3. args
+    with open(args.checkpoint_path + '/args.txt', 'w') as f:
+        for arg in vars(self.args):
+            val = getattr(self.args, arg)
+            if isinstance(val, str):
+                val = f"'{val}'"
+            f.write("{}: {}\n".format(arg, val))
+    np.save(self.args.checkpoint_path + "/args.npy", self.args)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
